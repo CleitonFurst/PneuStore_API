33da4c026372322050fcbd4ad9a9ba8085892ee4
CleitonFurst <43749359+CleitonFurst@users.noreply.github.com>
2021-09-29 22:07:55 -0300

    first commit

---
+++ PneuStore_API/Data/API_Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PneuStore_API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PneuStore_API.Data
+{
+    public class API_Context : IdentityDbContext
+    {
+        public API_Context(DbContextOptions<API_Context> options) 
+            : base (options) { }
+        public DbSet<Usuario> Usuarios { get; set;}
+
+    }
+}
