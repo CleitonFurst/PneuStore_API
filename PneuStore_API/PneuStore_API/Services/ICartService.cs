d3229192f33105772b6593937fefc15167eb2630
Italojztrindade <68620537+Italojztrindade@users.noreply.github.com>
2021-10-05 22:20:15 -0300

    ApiOk

---
+++ PneuStore_API/PneuStore_API/Services/ICartService.cs
@@ -0,0 +1,19 @@
+﻿using PneuStore_API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PneuStore_API.Services
+{
+    public interface ICartService
+    {
+        List<CartItem> All();
+        bool Create(CartItem c);
+        CartItem Get(int? id);
+        bool Update(CartItem c);
+        bool Delete(int? id);
+      
+        
+    }
+}
